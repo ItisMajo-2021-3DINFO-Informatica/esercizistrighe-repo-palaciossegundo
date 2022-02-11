cd9fcd5a26b79e53a72d435482042012d53b6e02
palaciossegundo <98151018+palaciossegundo@users.noreply.github.com>
2022-02-11 10:10:56 +0100

    esercizio stringhe3

---
+++ eserciziostringhe3/eserciziostringhe3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace eserciziostringhe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esercizio sulle stringhe 3");
+            string parola = "";
+            int contaparole = 0;
+            while (parola != "fine")
+            {
+                Console.WriteLine("Inserisci una parola");
+                parola = Console.ReadLine();
+                char prima = parola[0];
+                int lunghezza = parola.Length;
+                char ultima = parola[lunghezza - 1];
+                if (prima == ultima)
+                {
+                    contaparole++;
+                }
+                Console.WriteLine("Ci sono "+ contaparole + " che iniziano e finiscono per la stessa lettera");
+            }
+        }
+    }
+}
